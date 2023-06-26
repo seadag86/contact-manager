46afaabf660c6428a6b131efba5f432cd0ec7473
Myles Gregory <mylesbg@gmail.com>
2023-06-25 20:09:50 -0700

    Add login, registration and route protection

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+import { setError, superValidate } from 'sveltekit-superforms/server';
+import type { Actions, PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import { AuthApiError } from '@supabase/supabase-js';
+
+
+const registerUserSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 6 characters")
+  });
+  
+export const load: PageServerLoad = async (event) => {
+    const session = await event.locals.getSession();
+
+    if (session) { 
+        throw redirect(303, '/')
+    }
+    
+    return {
+        form: await superValidate(event, registerUserSchema)
+    }
+}
+
+export const actions: Actions = {
+    default: async (event) => {
+        const form = await superValidate(event, registerUserSchema);
+
+        if (!form.valid) {
+            return fail(400, { form});
+        }
+
+        const { error: authError } = await event.locals.supabase.auth.signInWithPassword(form.data)
+
+        if (authError) {
+            if (authError instanceof AuthApiError && authError.status === 401) {
+                setError(form, 'email', 'Invalid email or password.')
+                setError(form, 'password', 'Invalid email or password.')
+                fail(400, { form })
+            }
+        }
+
+        throw redirect(303, '/')
+    }
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import { Card, Button } from "flowbite-svelte";
+	import type { PageData } from "./$types";
+	import { superForm } from "sveltekit-superforms/client";
+
+	export let data: PageData;
+	const { form, errors, enhance } = superForm(data.form, {
+		resetForm: true
+	});
 </script>
 
 <div class="py-20">
@@ -10,14 +17,20 @@
 		</div>
 		<Card class="mt-6 w-full" padding="xl" size="md">
 			<!-- Login Form -->
-			<form class="flex flex-col space-y-6">
+			<form class="flex flex-col space-y-6" method="POST" use:enhance>
 				<label class="space-y-2" for="email">
 					<span>Email</span>
-					<input type="email" name="email" />
+					<input type="email" name="email" bind:value={$form.email} />
+					{#if $errors.email}
+						<div class="block text-red-600 dark:text-red-500">{$errors.email}</div>
+					{/if}
 				</label>
 				<label class="space-y-2" for="password">
 					<span>Password</span>
-					<input type="password" name="password" />
+					<input type="password" name="password" bind:value={$form.password} />
+					{#if $errors.password}
+						<div class="block text-red-600 dark:text-red-500">{$errors.password}</div>
+					{/if}
 				</label>
 				<Button type="submit" class="w-full">Login</Button>
 				<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
