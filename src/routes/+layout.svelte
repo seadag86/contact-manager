8f4023c8c0484ed59f2c31def8beaccb5de69394
Myles Gregory <mylesbg@gmail.com>
2023-06-26 17:04:15 -0700

    Add logout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import "../app.css";
 	import { page } from "$app/stores";
-	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button } from "flowbite-svelte";
+	import {
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		NavUl,
+		NavLi,
+		Button,
+		Dropdown,
+		DropdownItem,
+		Chevron
+	} from "flowbite-svelte";
 	import { onMount } from "svelte";
 	import { invalidate } from "$app/navigation";
 
 	const navigation = [
 		{ label: "Home", href: "/" },
 		{ label: "Pricing", href: "/pricing" },
-		{ label: "Contacts", href: "/contacts" },
-		{ label: "Account", href: "/account" }
+		{ label: "Contacts", href: "/contacts" }
 	];
 
 	export let data;
@@ -42,10 +51,24 @@
 			</span>
 		</NavBrand>
 		<div class="flex md:order-2">
-			<div class="flex items-center gap-2">
-				<Button href="/login" size="sm">Login</Button>
-				<Button href="/register" size="sm" color="alternative">Register</Button>
-			</div>
+			{#if session}
+				<Button color="light"><Chevron>Account</Chevron></Button>
+				<Dropdown>
+					<div slot="header" class="px-4 py-2">
+						<span class="block truncate text-sm font-medium"> {session.user.email} </span>
+					</div>
+					<DropdownItem href="/account">Settings</DropdownItem>
+					<DropdownItem href="/account">Billing</DropdownItem>
+					<form action="/logout" method="POST">
+						<DropdownItem type="submit" slot="footer">Sign out</DropdownItem>
+					</form>
+				</Dropdown>
+			{:else}
+				<div class="flex items-center gap-2">
+					<Button href="/login" size="sm">Login</Button>
+					<Button href="/register" size="sm" color="alternative">Register</Button>
+				</div>
+			{/if}
 			<NavHamburger on:click={toggle} />
 		</div>
 		<NavUl {hidden}>
